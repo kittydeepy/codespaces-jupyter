a12157d43f8e62464650e56823483cc9e339e62f
kittydeepy <francochalissery@gmail.com>
2023-01-16 09:48:31 +0000

    Changes for single linkedlist, append,prepend,insert_after,print_list

--- python/ds/linkedlist/linkedlistclass/linkedlist.py
+++ python/ds/linkedlist/linkedlistclass/linkedlist.py
@@ -2,15 +2,56 @@ class Node:
     """
     Class to create node
     """
+
     def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class LinkedList:
     """
     Class to create linkedlist
     """
+
     def __init__(self):
         self.head = None
 
-    def append(self, data)
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data)
+            curr_node = curr_node.next
+        print("\n")
+
+    def prepend(self, data):
+        curr_node = Node(data)
+        curr_node.next = self.head
+        self.head = curr_node
+
+    def insert_after(self, data, data_to_be_added):
+        new_node = Node(data_to_be_added)
+
+        if self.head is None:
+            return
+
+        curr_node = self.head
+        while curr_node:
+            if curr_node.data == data:
+                new_node.next = curr_node.next
+                curr_node.next = new_node
+                return
+            else:
+                curr_node = curr_node.next
+
+        
--- python/ds/linkedlist/singlelinkedlist.py
+++ python/ds/linkedlist/singlelinkedlist.py
@@ -1,6 +1,13 @@
-from linkedlistclass.linkedlist import Node, LinkedList
+from linkedlistclass.linkedlist import LinkedList
 
-def 
+def main():
+    linked_list = LinkedList()
+    linked_list.append("A")
+    linked_list.append("B")
+    linked_list.append(100)
+    linked_list.prepend("D")
+    linked_list.insert_after("B", "C")
+    linked_list.print_list()
 
 if __name__ == "__main__":
-    print("Yes, madam.")
+    main()
