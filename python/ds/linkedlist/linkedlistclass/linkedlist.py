6b47807cd10d0b18166ce515731d80a2f046c5ad
kittydeepy <francochalissery@gmail.com>
2023-01-20 07:13:58 +0000

    Single linked list with node swap and merge list

--- python/ds/linkedlist/linkedlistclass/linkedlist.py
+++ python/ds/linkedlist/linkedlistclass/linkedlist.py
@@ -72,7 +72,7 @@ class LinkedList:
                 curr_node = curr_node.next
 
     def delete_node_pos(self, pos):
-        curr_node = self.head
+        prev_node, curr_node = self.head
         count = 1
 
         if curr_node and pos == 0:
@@ -88,4 +88,115 @@ class LinkedList:
                 prev_node = curr_node
                 curr_node = curr_node.next
                 count += 1
+
+    def length_of_list_rec(self, node):
+        if node is None:
+            return 0
+        return 1 + self.length_of_list_rec(node.next)
      
+    def length_of_list(self):
+        curr_node = self.head
+        count = 0
+
+        while curr_node:
+            curr_node = curr_node.next
+            count += 1
+
+        return count
+    
+    def swap_nodes(self, key_1, key_2):
+
+        if key_1 == key_2:
+            return 
+
+        prev_1 = None 
+        curr_1 = self.head 
+        while curr_1 and curr_1.data != key_1:
+            prev_1 = curr_1 
+            curr_1 = curr_1.next
+
+        prev_2 = None 
+        curr_2 = self.head 
+        while curr_2 and curr_2.data != key_2:
+            prev_2 = curr_2 
+            curr_2 = curr_2.next
+
+        if not curr_1 or not curr_2:
+            return 
+
+        if prev_1:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
+
+        if prev_2:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_1
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
+
+    def reverse_list(self):
+        prev = None
+        curr = self.head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        self.head = prev
+
+    def reverse_list_rec(self):
+        def _recursive(curr, prev):
+            if not curr:
+                return prev
+
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            return _recursive(curr, prev)
+        self.head = _recursive(curr=self.head, prev=None)
+
+    def merge_list(self, list2):
+        p = self.head
+        q = list2
+
+        s = None
+
+        if not p:
+            return q
+        elif not q:
+            return p
+
+        if p.data <= q.data:
+            s = p
+            p = p.next
+        else:
+            s = q
+            q = q.next
+        new_head = s
+
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+
+        if not q:
+            s.next = p
+        else:
+            s.next = q
+        
+        return self.head = new_head
+
+            
+
+
+
+        
--- python/ds/linkedlist/singlelinkedlist.py
+++ python/ds/linkedlist/singlelinkedlist.py
@@ -4,9 +4,18 @@ def main():
     linked_list = LinkedList()
     linked_list.append("A")
     linked_list.append("B")
-    linked_list.append(100)
-    linked_list.prepend("D")
-    linked_list.insert_after("B", "C")
+    linked_list.append("C")
+    linked_list.append("D")
+    linked_list.print_list()
+    linked_list.reverse_list()
+    linked_list.print_list()
+    linked_list.reverse_list_rec()
+    linked_list.print_list()
+    linked_list.prepend(100)
+    print(linked_list.length_of_list())
+    print(linked_list.length_of_list_rec(linked_list.head))
+    '''
+    linked_list.insert_after(200, "E")
     linked_list.print_list()
     linked_list.delete_node_value("C")
     linked_list.print_list()
@@ -14,6 +23,7 @@ def main():
     linked_list.print_list()
     linked_list.delete_node_pos(3)
     linked_list.print_list()
+    '''
 
 if __name__ == "__main__":
     main()
