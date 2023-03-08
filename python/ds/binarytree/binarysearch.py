351779563a008a921eefe6d4525322fd2f6da928
kittydeepy <francochalissery@gmail.com>
2023-03-08 02:57:56 +0000

    binary search iterative, recursive, linear

--- python/ds/binarytree/binarysearch.py
+++ python/ds/binarytree/binarysearch.py
@@ -1,11 +1,21 @@
-def binarysearch(data, target, low, high):
+'''
+Complexities is O(n)
+'''
+def binarysearchlinear(data, target):
+    for i in range(len(data) - 1):
+        if data[i] == target:
+            return True
+    return False
+
+'''
+Complexities is O(log n)
+'''
+def binarysearchiterative(data, target, low, high):
 
     low = 0
     high = len(data) - 1
     
-    if low > high:
-        return False
-    else:
+    while low <= high:
         mid = (low + high)// 2
         if target == data[mid]:
             return True
@@ -15,3 +25,18 @@ def binarysearch(data, target, low, high):
             low = mid + 1 
 
     return False
+
+"""
+complexities O(log n)
+"""
+def binarysearchrecursive(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high)//2
+        if target == data[mid]:
+            return True
+        elif target > data[mid] :
+            binarysearchrecursive(data, target, mid + 1, high)
+        else:
+            binarysearchrecursive(data, target, low, mid-1)
